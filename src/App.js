3b678c8e65e548c92e45a0f78a26070a0409535c
ericsonmoreira <moreira.ericson@gmail.com>
2020-09-19 20:04:25 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Grid,
+  LinearProgress,
+  makeStyles,
+  Paper,
+} from '@material-ui/core';
+import { Field, Form, Formik } from 'formik';
+import { TextField } from 'formik-material-ui';
+import * as Yup from 'yup';
+
+const initialValues = {
+  email: '',
+  password: '',
+};
+
+const schema = Yup.object().shape({
+  email: Yup.string().email('Email inválido').required('Campo obrigatório'),
+  password: Yup.string().required('Campo obrigatório'),
+});
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    height: '100vh',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            setSubmitting(false);
+            alert(JSON.stringify(values, null, 2));
+          }, 500);
+        }}
+        validationSchema={schema}
+      >
+        {({ submitForm, isSubmitting }) => (
+          <Form>
+            <Grid
+              alignItems="center"
+              component={Paper}
+              container
+              direction="column"
+              justify="center"
+              spacing={2}
+            >
+              <Grid item>
+                <Field
+                  component={TextField}
+                  label="Email"
+                  name="email"
+                  type="email"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item>
+                <Field
+                  component={TextField}
+                  label="Password"
+                  name="password"
+                  type="password"
+                  variant="outlined"
+                />
+                {isSubmitting && (
+                  <Grid item>
+                    <LinearProgress />
+                  </Grid>
+                )}
+              </Grid>
+              <Grid item>
+                <Button
+                  color="primary"
+                  disabled={isSubmitting}
+                  fullWidth
+                  onClick={submitForm}
+                  variant="contained"
+                >
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
